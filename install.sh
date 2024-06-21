1ed7a36d11fd6df5b73c99a591af1c3300be4e74
wst <91311843+AlexRar@users.noreply.github.com>
2024-06-21 12:04:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+read -n1 -rep 'Would you like to install the packages? (y,n)' INST
+if [[ $INST == "Y" || $INST == "y" ]]; then
 sudo pacman -Syu
 sudo pacman -S --needed base-devel git
 git clone https://aur.archlinux.org/yay.git
